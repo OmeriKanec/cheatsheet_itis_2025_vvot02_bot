b8346a412b20bbf683cd21ffcbcb55cde7020056
GRLazarev <GRLazarev@kpfu.ru>
2025-11-14 14:06:10 +0300

    Basic bot added

---
+++ src/main/java/bot/Bot.java
@@ -0,0 +1,42 @@
+package bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "Шпаргалка ИТИС 2025 vvot02";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "token";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var message = update.getMessage();
+        var user = message.getFrom();
+        var id = user.getId();
+        if (message.isCommand()) {
+            if (message.getText().equals("/start") || message.getText().equals("/help")) {
+                sendText(id, "Я помогу ответить на экзаменационный вопрос по «Операционным системам».\n" +
+                        "Присылайте вопрос — фото или текстом.");
+            }
+        }
+    }
+
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString())
+                .text(what).build();
+        try {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
