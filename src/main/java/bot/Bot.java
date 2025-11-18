a52d674e99f028c377bc13c0d61ef767456bb462
GRLazarev <GRLazarev@kpfu.ru>
2025-11-18 14:31:52 +0300

    Обновил метод отправки сообщения для работы через http

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -1,13 +1,20 @@
 package bot;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Bot extends TelegramWebhookBot {
+
+    private static final String TELEGRAM_BOT_TOKEN = System.getenv("TELEGRAM_BOT_TOKEN");
+    private static final String BOT_PATH = "webhook";
+
     @Override
     public String getBotUsername() {
         return "Шпаргалка ИТИС 2025 vvot02";
@@ -15,16 +22,27 @@ public class Bot extends TelegramWebhookBot {
 
     @Override
     public String getBotToken() {
-        return Dotenv.load().get("TOKEN");
+        return TELEGRAM_BOT_TOKEN;
     }
 
-    public void sendText(Long who, String what){
-        SendMessage sm = SendMessage.builder()
-                .chatId(who.toString())
-                .text(what).build();
+    public void sendText(Long chatId, String text) {
+        String url = "https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendMessage";
+
+        String json = String.format(
+                "{\"chat_id\": \"%s\", \"text\": \"%s\"}",
+                chatId, text.replace("\"", "\\\"")
+        );
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
         try {
-            execute(sm);
-        } catch (TelegramApiException e) {
+            client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
@@ -45,6 +63,6 @@ public class Bot extends TelegramWebhookBot {
 
     @Override
     public String getBotPath() {
-        return null;
+        return BOT_PATH;
     }
 }
