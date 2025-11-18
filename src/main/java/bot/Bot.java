008076654775d2ac31b719d621ad3bc95ec7b346
GRLazarev <GRLazarev@kpfu.ru>
2025-11-18 12:50:02 +0300

    Понял, что делал не тот тип бота, забыл закоммитить до этого

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -1,12 +1,13 @@
 package bot;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class Bot extends TelegramLongPollingBot {
+public class Bot extends TelegramWebhookBot {
     @Override
     public String getBotUsername() {
         return "Шпаргалка ИТИС 2025 vvot02";
@@ -15,19 +16,6 @@ public class Bot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return Dotenv.load().get("TOKEN");
-    }
-
-    @Override
-    public void onUpdateReceived(Update update) {
-        var message = update.getMessage();
-        var user = message.getFrom();
-        var id = user.getId();
-        if (message.isCommand()) {
-            if (message.getText().equals("/start") || message.getText().equals("/help")) {
-                sendText(id, "Я помогу ответить на экзаменационный вопрос по «Операционным системам».\n" +
-                        "Присылайте вопрос — фото или текстом.");
-            }
-        }
     }
 
     public void sendText(Long who, String what){
@@ -39,5 +27,24 @@ public class Bot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        var message = update.getMessage();
+        var user = message.getFrom();
+        var id = user.getId();
+        if (message.isCommand()) {
+            if (message.getText().equals("/start") || message.getText().equals("/help")) {
+                sendText(id, "Я помогу ответить на экзаменационный вопрос по «Операционным системам».\n" +
+                        "Присылайте вопрос — фото или текстом.");
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getBotPath() {
+        return null;
     }
 }
--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -1,12 +1,13 @@
 package bot;
 
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(new Bot());
+        botsApi.registerBot(new Bot(), new SetWebhook());
     }
 }
